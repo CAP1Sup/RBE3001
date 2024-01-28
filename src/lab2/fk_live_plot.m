f7fd5c76f2012ef3ba3c0b1ad0e7acb056cbf7d8
Jack Kamataris <jackk2003@icloud.com>
2024-01-28 11:16:07 -0500

    Cleaned up plotting code to lab report standards

--- src/lab2/fk_live_plot.m
+++ src/lab2/fk_live_plot.m
@@ -1,13 +1,13 @@
 %% Setup robot
-travelTime = 10; % Defines the travel time
+travelTime = 5; % Defines the travel time
 model = Model(); % Creates model object
 waypoint = [45, -55, -50,  90;
             20,  20,  20,  20;
             10, -20,  15, -12;
            -80, -70,   5,   0;
-           -30,  30,  10,  45]; % Give the angle for each position
+           -30,  30,  10,  -45]; % Give the angle for each position
 home = [0,0,0,0]; % Home 0,0,0,0 position
-trialNum = 1; %% SET THIS NUMBER FOR EACH INDIVIDUAL TRIAL
+trialNum = 5; %% SET THIS NUMBER FOR EACH INDIVIDUAL TRIAL
 
 %% Move the robot back to the home
 model.robot.set_joint_vars(home, 3000);
@@ -22,4 +22,10 @@ tic;
 while toc <= travelTime
     joint_pos = model.robot.read_joint_vars(true, false);
     model.plot_arm(joint_pos(1, :), robotPlot);
+    xlabel('x pos (mm)')
+    ylabel('y pos (mm)')
+    zlabel('z pos (mm)')
+    grid("on")
+    title("3D arm model at position " + trialNum)
+    set(gca,'fontsize',16)
 end
