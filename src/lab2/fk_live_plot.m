14e25d839e1058e3b42acdab65a5c23a8d97f841
Christian Piper <xmanpiper@gmail.com>
2024-01-29 20:43:34 -0500

    Added reference frames to 3d arm plots Also added JPEG of robot for lab

--- src/lab2/fk_live_plot.m
+++ src/lab2/fk_live_plot.m
@@ -1,31 +1,35 @@
 %% Setup robot
 travelTime = 10; % Defines the travel time
 model = Model(); % Creates model object
-waypoint = [45, -55, -50,  90;
-            20,  20,  20,  20;
-            10, -20,  15, -12;
-           -80, -70,   5,   0;
-           -30,  30,  10,  -45]; % Give the angle for each position
-home = [0,0,0,0]; % Home 0,0,0,0 position
-trialNum = 1; %% SET THIS NUMBER FOR EACH INDIVIDUAL TRIAL
+waypoint = [45, -55, -50, 90;
+            20, 20, 20, 20;
+            10, -20, 15, -12;
+            -80, -70, 5, 0;
+            -30, 30, 10, -45]; % Give the angle for each position
+home = [0, 0, 0, 0]; % Home 0,0,0,0 position
+trialNum = 1; % % SET THIS NUMBER FOR EACH INDIVIDUAL TRIAL
 
 %% Move the robot back to the home
 model.robot.set_joint_vars(home, 3000);
 pause(3);
 
 %% Create the plot, perform the movement
-robotPlot = model.new_arm_plot();
-model.robot.set_joint_vars(waypoint(trialNum, :), travelTime*1000);
+close all
+figure(1)
+title("3D Arm Live Model at Position " + trialNum)
+xlabel("X");
+ylabel("Y");
+zlabel("Z");
+hold on
+grid on
+[robotPlot, xQPlot, yQPlot, zQPlot] = model.new_arm_plots();
+hold off
+model.robot.set_joint_vars(waypoint(trialNum, :), travelTime * 1000);
 
 % Start the timer
 tic;
+
 while toc <= travelTime
     joint_pos = model.robot.read_joint_vars(true, false);
-    model.plot_arm(joint_pos(1, :), robotPlot);
-    xlabel('x pos (mm)')
-    ylabel('y pos (mm)')
-    zlabel('z pos (mm)')
-    grid("on")
-    title("3D arm model at position " + trialNum)
-    set(gca,'fontsize',16)
+    model.plot_arm(joint_pos(1, :), robotPlot, xQPlot, yQPlot, zQPlot);
 end
--- src/lib/Model.m
+++ src/lib/Model.m
@@ -11,18 +11,34 @@ classdef Model
         end
 
         % Creates a new plot for displaying the arm
-        function robotPlot = new_arm_plot(~)
-            robotPlot = plot3(0, 0, 0, "-r.");
-            axis([-300, 300, -300, 300, 0, 400]);
+        function [robotPlot, xQPlot, yQPlot, zQPlot] = new_arm_plots(~)
+            hold on
+            robotPlot = plot3(0, 0, 0, 1, 1, 1, "-r.");
+            scaleFactor = 0.35;
+            xQPlot = quiver3(0, 0, 0, 0, 0, 0, Color="r", AutoScaleFactor=scaleFactor);
+            yQPlot = quiver3(0, 0, 0, 0, 0, 0, Color="g", AutoScaleFactor=scaleFactor);
+            zQPlot = quiver3(0, 0, 0, 0, 0, 0, Color="b", AutoScaleFactor=scaleFactor);
+            axis([-300, 300, -300, 300, -50, 400]);
+            hold off
         end % new_arm_plot
 
         % Takes a 1x4 array of joint values and the plot to update
         % Plots a stick model of the arm showing all frames, joints, and links
-        function plot_arm(self, joint_pos, robotPlot)
+        function plot_arm(self, joint_pos, robotPlot, xQPlot, yQPlot, zQPlot)
             fk = self.robot.joints2fk(joint_pos);
-            set(robotPlot, "XData", fk(1,4,:), "YData", fk(2,4,:), "ZData", fk(3,4,:));
+            xs = squeeze(fk(1,4,:));
+            ys = squeeze(fk(2,4,:));
+            zs = squeeze(fk(3,4,:));
+            set(robotPlot, "XData", xs, "YData", ys, "ZData", zs);
+
+            xhats = transpose(squeeze(fk(1:3,1,:)));
+            yhats = transpose(squeeze(fk(1:3,2,:)));
+            zhats = transpose(squeeze(fk(1:3,3,:)));
+            set(xQPlot, "XData", xs, "YData", ys, "ZData", zs, "UData", xhats(:,1), "VData", xhats(:,2), "WData", xhats(:,3));
+            set(yQPlot, "XData", xs, "YData", ys, "ZData", zs, "UData", yhats(:,1), "VData", yhats(:,2), "WData", yhats(:,3));
+            set(zQPlot, "XData", xs, "YData", ys, "ZData", zs, "UData", zhats(:,1), "VData", zhats(:,2), "WData", zhats(:,3));
             drawnow
         end % plot_arm
 
     end % end methods
-end % end class 
+end % end class
