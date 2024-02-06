f15fd8c2d57a389817bc460cb6fa311888150581
Jack Kamataris <jackk2003@icloud.com>
2024-02-06 12:21:27 -0500

    Added run traj planning in task space

--- src/lab3/run_task_space_traj_testing.m
+++ src/lab3/run_task_space_traj_testing.m
@@ -1,9 +1,56 @@
+clear;
+%% SETUP
 robot = Robot();
 
-move_time = 3;
-robot.set_joint_vars([45,0,0,-45], 3*1000);
-coeff = calc_t_coeff(robot, [200, 0, 100, 0], move_time);
-robot.run_trajectory(coeff, move_time, true)
+location = [200,  0,   100,  0;
+            100,  200, 350, -90;
+            50,  -70,  300, -45;
+            200,  0,   100,  0];
+
+travelTime = 3; % seconds
+maxTrials = 3; %% SET THIS NUMBER FOR EACH INDIVIDUAL TRIAL
+currTrial = 1; % initalize trial start
+
+%% CODE
+
+robot.set_joint_vars([0, 0, 0, 0], travelTime*1000);
+pause(travelTime)
+ik_pose = robot.task2ik(location(currTrial,:));
+robot.set_joint_vars(ik_pose,travelTime*1000);
+pause(travelTime);
+
+for trial = currTrial:currTrial+2
+    coeffs = calc_t_coeff(robot,location(trial+1,:), travelTime);
+    new_joint_pos = robot.run_trajectory(coeffs, travelTime, true);
+    if exist("prev_joint_pos", "var")
+        new_joint_pos(:,1) = new_joint_pos(:,1) + max(prev_joint_pos(:,1));
+    end
+    if exist("joint_pos", "var")
+        joint_pos = [joint_pos; new_joint_pos];
+    else
+        joint_pos = new_joint_pos;
+    end
+    prev_joint_pos = new_joint_pos;
+end % for
+
+
+figure(1)
+plot(joint_pos(:,1), joint_pos(:,2));
+hold on
+plot(joint_pos(:,1), joint_pos(:,3));
+plot(joint_pos(:,1), joint_pos(:,4));
+plot(joint_pos(:,1), joint_pos(:,5));
+hold off
+legend('Base angle', 'Shoulder Angle', 'Elbow Angle','Wrist Angle', 'Location', 'northwest')
+title('Joint Angles Cubic Trajectory of a Triangle From Inverse Kinematics in Task Space')
+xlabel('Time (s)')
+ylabel('Pos (deg)')
+axis([0 max(joint_pos(:,1)) -100 100])
+set(gca,'fontsize',16);
+
+
+
+
 
 
 % Conveience function to generate the task space trajectory coefficients
